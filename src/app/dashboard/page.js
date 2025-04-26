0b95fb163f7a5e9654169ab29dedbc6ecf2ef865
Bianca <bianca.dennis92@gmail.com>
2025-04-26 17:34:21 -0400

    wip

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,117 @@
+'use client'
+import {useEffect} from 'react'
+import Image from "next/image";
+import styles from "../page.module.css";
+import getRecipeData from '@dimfu/recipe-scraper'
+
+// const URL_SUBSTRING_FOR_CORS_ISSUE = 'https://cors-anywhere.herokuapp.com'
+const RECIPE_LINK = 'https://www.allrecipes.com/recipe/268091/easy-korean-ground-beef-bowl'
+
+export default function Home() {
+    async function parseRecipe() {
+        // try {
+        //     const recipe = await getRecipeData(`${URL_SUBSTRING_FOR_CORS_ISSUE}/${RECIPE_LINK}`)
+        //     console.log('recipe', recipe)
+        // } catch(e) {
+        //     console.log('catch', e)
+        // }
+
+  const recipe = await getRecipeData(RECIPE_LINK)
+  console.log(recipe)
+     
+      }
+
+    useEffect(() => {
+        parseRecipe()
+    }, [])
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantlyy.</li>
+        </ol>
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
